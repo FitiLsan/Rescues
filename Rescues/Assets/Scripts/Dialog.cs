7c9205c0a56778b78a754746e8116c723b8988eb
Fitilsan <fitil_san@mail.ru>
2019-10-07 23:55:46 +0300

    Upload project

---
+++ Rescues/Assets/Scripts/Dialog.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : Interactable
+{
+    public GameObject _dialogSprite;
+
+    public override void Interact(Transform go)
+    {
+        _dialogSprite.SetActive(!_dialogSprite.activeInHierarchy);
+    }
+}
---
+++ Rescues/Assets/Scripts/DoorTeleporter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTeleporter : MonoBehaviour
+{
+    [SerializeField] Transform upperDoor;
+    [SerializeField] Transform lowerDoor;
+
+
+    public void JumpUp(Transform go)
+    {
+        if(upperDoor != null) go.transform.position = upperDoor.position;
+    }
+
+    public void JumpDown(Transform go)
+    {        
+        if (lowerDoor != null) go.transform.position = lowerDoor.position;
+    }
+}
