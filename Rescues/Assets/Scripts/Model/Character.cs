3c59e202f5f2dd8f5d9008dd3661844f7bb6a61e
FeodorG <FeodorG2505@mail.ru>
2019-12-06 19:36:36 +0300

    Player Movement

--- Rescues/Assets/Scripts/Model/Character.cs
+++ Rescues/Assets/Scripts/Model/Character.cs
@@ -5,14 +5,27 @@ namespace Rescues
 {
     public sealed class Character
     {
+        #region PrivateData
         private Vector3 _direction;
         private readonly float _speed;
+        private float _horizontal;
         private Rigidbody2D Rigidbody2D { get; }
         private Transform Transform { get; }
         private PlayerBehaviour PlayerBehaviour { get; }
 
         private bool _isForward = true;
-
+        #endregion
+        #region UnityMethods
+        void Update()
+        {
+            _horizontal = Input.GetAxis("Horizontal");
+            if(_horizontal != 0)
+            {
+                Move(1);
+            }
+        }
+        #endregion
+        #region Methods
         public Character(Transform transform, PlayerData playerData)
         {
             _direction = Vector3.zero;
@@ -23,7 +36,10 @@ namespace Rescues
         }
 
         public void Move(float direction)
-        {        
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Transform.position, Vector2.down, 9);
+            if (hit != true) Transform.Translate(new Vector3(0.0f, -0.1f, 0.0f));
+            Transform.Translate(new Vector3(_horizontal, 0.0f, 0.0f));
             _direction.x = direction * _speed;
             _direction.y = Rigidbody2D.velocity.y;
 
@@ -33,11 +49,13 @@ namespace Rescues
             {
                 Flip();
             }
-            else if(Rigidbody2D.velocity.x < 0 && _isForward)
+            else if (Rigidbody2D.velocity.x < 0 && _isForward)
             {
                 Flip();
-            }
+            
+  
         }
+    }
 
         private void Flip()
         {
@@ -46,5 +64,6 @@ namespace Rescues
             dir.x *= -1;
             Transform.localScale = dir;
         }
+        #endregion
     }
 }
