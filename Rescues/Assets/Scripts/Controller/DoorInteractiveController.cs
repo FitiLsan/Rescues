c2ea4a11c8d2c8384867eff22fd7067b66f50047
StepaChe <47980998+StepaChe@users.noreply.github.com>
2020-01-19 18:35:46 +0300

    Revert "Revert "started developing door opening""
    This reverts commit b50fdd2af4af821db89cd96c52659917d33d9410.

---
+++ Rescues/Assets/Scripts/Controller/DoorInteractiveController.cs
@@ -0,0 +1,76 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+
+namespace Rescues
+{
+    public sealed class DoorInteractiveController : IInitializeController, ITearDownController
+    {
+        #region Fields
+        
+        private readonly GameContext _context;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+        
+        public DoorInteractiveController(GameContext context, Services services)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+
+        #region IInitializeController
+        
+        public void Initialize()
+        {
+            var doors = _context.GetTriggers<DoorInteractiveBehaviour>(InteractableObjectType.Door);
+            foreach (var trigger in doors)
+            {
+                trigger.OnTriggerEnterHandler += OnTriggerEnterHandler;
+                trigger.OnTriggerExitHandler += OnTriggerExitHandler;
+            }
+        }
+
+        #endregion
+
+
+        #region ITearDownController
+        
+        public void TearDown()
+        {
+            var doors = _context.GetTriggers<DoorInteractiveBehaviour>(InteractableObjectType.Door);
+            foreach (var trigger in doors)
+            {
+                trigger.OnTriggerEnterHandler -= OnTriggerEnterHandler;
+                trigger.OnTriggerExitHandler -= OnTriggerExitHandler;
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+        
+        private void OnTriggerEnterHandler(ITrigger obj)
+        {
+            obj.IsInteractable = true;
+            var materialColor = obj.GameObject.GetComponent<SpriteRenderer>().color;
+            obj.GameObject.GetComponent<SpriteRenderer>().DOColor(new Color(materialColor.r,
+                materialColor.g, materialColor.b, 0.5f), 1.0f);
+        }
+
+        private void OnTriggerExitHandler(ITrigger obj)
+        {
+            obj.IsInteractable = false;
+            var materialColor = obj.GameObject.GetComponent<SpriteRenderer>().color;
+            obj.GameObject.GetComponent<SpriteRenderer>().DOColor(new Color(materialColor.r,
+                materialColor.g, materialColor.b, 1.0f), 1.0f);
+        }
+
+        #endregion
+    }
+}
