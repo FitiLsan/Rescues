b572b940793d8188cd358b94e56ac172973a850f
FeodorG <FeodorG2505@mail.ru>
2020-01-23 22:19:48 +0300

    Hide-and-seek

---
+++ Rescues/Assets/Scripts/Model/Behaviour/HideBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+namespace Rescues
+{
+    public sealed class HideBehaviour : InteractableObjectBehavior
+    {
+        #region Fields
+
+        [SerializeField] public Transform ExitPoint;
+        private ITrigger _triggerImplementation;
+
+        #endregion
+    }
+}
