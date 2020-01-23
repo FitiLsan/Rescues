c0bb53719cffc220d7e72a8a348723cd34630197
FeodorG <FeodorG2505@mail.ru>
2020-01-23 19:02:07 +0300

    Hide-and-seek
    Добавил HideController и InitializeHideController

---
+++ Rescues/Assets/Scripts/Controller/HideController.cs
@@ -0,0 +1,85 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+
+namespace Rescues
+{
+    public sealed class HideController : IInitializeController, ITearDownController
+    {
+        #region Fields
+
+        private readonly GameContext _context;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public HideController(GameContext context, Services services)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+
+        #region IInitializeController
+
+        public void Initialize()
+        {
+            var hideObj = _context.GetTriggers(InteractableObjectType.HideObj);
+            foreach (var trigger in hideObj)
+            {
+                var HideController = trigger as HideController;
+                HideController.OnFilterHandler += OnFilterHandler;
+                HideController.OnTriggerEnterHandler += OnTriggerEnterHandler;
+                HideController.OnTriggerExitHandler += OnTriggerExitHandler;
+            }
+        }
+
+        #endregion
+
+
+        #region ITearDownController
+
+        public void TearDown()
+        {
+            var hideObj = _context.GetTriggers(InteractableObjectType.Door);
+            foreach (var trigger in hideObj)
+            {
+                var HideController = trigger as HideController;
+                HideController.OnFilterHandler -= OnFilterHandler;
+                HideController.OnTriggerEnterHandler -= OnTriggerEnterHandler;
+                HideController.OnTriggerExitHandler -= OnTriggerExitHandler;
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private bool OnFilterHandler(Collider2D obj)
+        {
+            return obj.CompareTag(TagManager.PLAYER);
+        }
+
+        private void OnTriggerEnterHandler(ITrigger enteredObject)
+        {
+            enteredObject.IsInteractable = true;
+            var materialColor = enteredObject.GameObject.GetComponent<SpriteRenderer>().color;
+            enteredObject.GameObject.GetComponent<SpriteRenderer>().DOColor(new Color(materialColor.r,
+                materialColor.g, materialColor.b, 0.5f), 1.0f);
+        }
+
+        private void OnTriggerExitHandler(ITrigger enteredObject)
+        {
+            enteredObject.IsInteractable = false;
+            var materialColor = enteredObject.GameObject.GetComponent<SpriteRenderer>().color;
+            enteredObject.GameObject.GetComponent<SpriteRenderer>().DOColor(new Color(materialColor.r,
+                materialColor.g, materialColor.b, 1.0f), 1.0f);
+        }
+
+        #endregion
+    }
+}
---
+++ Rescues/Assets/Scripts/Controller/InitializeHideController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+
+namespace Rescues
+{
+    public sealed class InitializeHideController : IInitializeController
+    {
+        #region Fields
+
+        private readonly GameContext _context;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public HideController(GameContext context, Services services)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+
+        #region IInitializeController
+
+        public void Initialize()
+        {
+            var triggers = Object.FindObjectsOfType<HideBehaviour>();
+
+            foreach (var trigger in triggers)
+            {
+                trigger.OnFilterHandler = OnFilterHandler;
+                _context.AddTriggers(InteractableObjectType.HideObj, trigger);
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private bool OnFilterHandler(Collider2D obj)
+        {
+            return obj.CompareTag(TagManager.PLAYER);
+        }
+
+        #endregion
+    }
+}
