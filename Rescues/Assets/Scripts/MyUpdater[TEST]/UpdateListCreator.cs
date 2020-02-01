6dd43d4a15cad75c0d3d12b399f3c343f81797a9
treopotr <treopotr@gmail.com>
2020-02-01 17:49:09 +0200

    replased method Update in GameController on method in "UpdateListCreator"

---
+++ Rescues/Assets/Scripts/MyUpdater[TEST]/UpdateListCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class UpdateListCreator : MonoBehaviour
+{
+
+    public static List<UpdateListCreator> updateList = new List<UpdateListCreator>();
+
+
+    private void OnEnable()
+    {
+        updateList.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        updateList.Remove(this);
+    }
+
+    public virtual void GameControllerScriptFunctions()
+    {
+
+    }
+
+
+}
---
+++ Rescues/Assets/Scripts/MyUpdater[TEST]/UpdateListStarter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateListStarter : MonoBehaviour
+{
+    
+    void Update()
+    {
+        
+        for(int i = 0; i < UpdateListCreator.updateList.Count; i++)
+        {
+
+            UpdateListCreator.updateList[i].GameControllerScriptFunctions();
+
+        }
+
+
+    }
+}
