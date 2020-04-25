eb7e33e79e74616a7f9dcab32252cf29886af675
valera <valera.basarov@mail.ru>
2020-04-25 12:25:59 +0300

    code/valera-basarov/zadanie1,2
    Адапционная задача
    Сигнализация

---
+++ Rescues/Assets/SignalingBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SignalingBehaviour : MonoBehaviour
+{
+    public AudioClip hitsound;
+    AudioSource audio;
+
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //событие
+            audio.PlayOneShot(hitsound);
+        }
+    }
+}
---
+++ Rescues/Assets/kontroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kontroller : MonoBehaviour
+{
+    public float speed = 30f;
+
+    Vector3 dir = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        dir.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += dir;
+
+
+        
+    }
+}
