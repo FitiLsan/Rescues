9d5598e3b3a1a141dc845c0c972c99d309ae5001
valera <valera.basarov@mail.ru>
2020-05-02 14:45:36 +0300

    code/valera-basarov/zadanie2
    Сигнализация

---
+++ Rescues/Assets/Menedger.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+
+
+
+
+public delegate void MyEventHandler();
+public class Menedger : MonoBehaviour
+{
+
+
+    public static AudioClip Hitsound;
+    public static AudioSource Audio;
+
+    public event MyEventHandler SomeEvent;
+
+
+    public void OnSomeEvent()
+    {
+        if (SomeEvent != null)
+            SomeEvent();
+    }
+
+   public static void Handler()
+    {
+
+        Audio.PlayOneShot(Hitsound);
+    }
+
+
+
+
+    void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+        Menedger evt = new Menedger();
+        evt.SomeEvent += Handler;
+    }
+
+   
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //событие
+            OnSomeEvent();
+
+            
+            
+        }
+    }
+}
