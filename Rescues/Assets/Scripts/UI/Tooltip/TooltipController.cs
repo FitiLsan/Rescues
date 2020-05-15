2e2ea1b3373d15b1d89f4aaaea0ea12ad2c48e68
FeodorG <FeodorG2505@mail.ru>
2020-05-15 20:27:12 +0300

    UI tips
    Made UI tips

---
+++ Rescues/Assets/Scripts/UI/Tooltip/TooltipController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Rescues
+{
+    public sealed class TooltipController : IExecuteController
+    {
+
+        #region Fields
+
+        private readonly GameContext _context;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public TooltipController(GameContext context, Services services)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        #region IExecuteController
+
+        public void Execute()
+        {
+        }
+
+
+        #endregion
+
+        private T GetInteractableObject<T>(InteractableObjectType type) where T : class
+        {
+            var interactableObjects = _context.GetTriggers(type);
+            T behaviour = default;
+
+            foreach (var trigger in interactableObjects)
+            {
+                if (trigger.IsInteractable)
+                {
+                    CustomDebug.Log(trigger.Description) ;
+                }
+            }
+
+            return behaviour;
+        }
+    }
+}
