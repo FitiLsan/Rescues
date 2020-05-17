6bc3291e9d225015277d4985afb52ac580abff36
holylighto <forceoffear1@gmail.com>
2020-05-17 20:32:51 +0300

    fog of war
    Пока не работает

---
+++ Rescues/Assets/FOG/FieldOfViewController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Rescues
+{
+    public class FieldOfViewController : MonoBehaviour
+    {
+        private bool _isEnabled = true;
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("ViewObstacle"))
+            {
+                _isEnabled = false;
+            }
+            else if (collision.gameObject.CompareTag("FogOfWar") && _isEnabled)
+            {
+                var fog = collision.GetComponent<FogOfWarBehaviour>();
+                fog.FogEnter();
+            }
+        }
+
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("ViewObstacle"))
+            {
+                _isEnabled = true;
+            }
+            else if (collision.gameObject.CompareTag("FogOfWar"))
+            {
+                var fog = collision.GetComponent<FogOfWarBehaviour>();
+                fog.FogLeft();
+            }
+        }
+    }
+}
